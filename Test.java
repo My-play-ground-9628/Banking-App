2672d3f5a0fd9dbe1cbfa39ee40c765f190ecdc2
shashikala <chandrasekaremsmh@gmail.com>
2023-08-16 02:39:34 +0530

    Done withdraw money

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        String str = "SDB-00001";
-        System.out.println(Integer.valueOf(str.substring(8)));
-
-    }
-    
-}
