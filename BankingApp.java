305a14cdc08322f69eb47383148f4042ee4bb0b9
shashikala <chandrasekaremsmh@gmail.com>
2023-08-15 23:30:04 +0530

    Created new accounts

---
+++ BankingApp.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class BankingApp {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+        final String clear = "\033[H\033[2J";
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        final String DASHBOARD = "Welcome to Smart Banking";
+        final String CREATE_NEW_ACCOUNT = "Open new account";
+        final String DEPOSIT = "Deposit money";
+        final String WITHDRAWS = "Withdraw money";
+        final String TRANSFER = "Transfer money";
+        final String CHECK_ACCOUNT_BALANCE = "Check Account Balance";
+        final String DELETE_ACCOUNT = "Drop existing account";
+
+        String[] names = new String[0];
+        int[] deposits = new int[0];
+
+        String screen = DASHBOARD;
+
+        do{
+            final String APP_TITLE = String.format("%s%s%s", COLOR_BLUE_BOLD, screen, RESET);
+            System.out.println(clear);
+            System.out.println("\t" + APP_TITLE + "\n");
+            
+            switch(screen){
+                case DASHBOARD:
+                    System.out.println("\t[1]. Open New Account");
+                    System.out.println("\t[2]. Deposit Money");
+                    System.out.println("\t[3]. Withdraw Money");
+                    System.out.println("\t[4]. Transfer Money");
+                    System.out.println("\t[5]. Check Account Balance");
+                    System.out.println("\t[6]. Drop Existing Account");
+                    System.out.println("\t[4]. Exit\n");
+                    System.out.print("Enter an Option to Continue > ");
+
+                    int option = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (option) {
+                        case 1: screen = CREATE_NEW_ACCOUNT; break;
+                        case 2: screen = DEPOSIT; break;
+                        case 3: screen = WITHDRAWS; break;
+                        case 4: screen = TRANSFER; break;
+                        case 5: screen = CHECK_ACCOUNT_BALANCE; break;
+                        case 6: screen = DELETE_ACCOUNT; break;
+                        case 7: System.out.println(clear); System.exit(0); break;
+                    }
+                    break;
+
+                case CREATE_NEW_ACCOUNT:
+                    String name;
+                    int initialDeposit;
+                    boolean valid = true;
+                    System.out.printf("ID: SDB-%05d \n", (names.length + 1));
+                    
+                    //Name validation 
+                    do{
+                        valid = true;
+                        System.out.println("\tEnter Customer Name: ");
+                        name = scanner.nextLine().toUpperCase();
+                        if (name.isBlank()) {
+                            System.out.printf(ERROR_MSG , "Name Can't be empty");
+                            valid = false;
+                            continue;
+                        }else {
+                            for (int i = 0; i < name.length(); i++) {
+                                if (!(Character.isLetter(name.charAt(i)) || Character.isSpaceChar(name.charAt(i)))) {
+                                    System.out.printf(ERROR_MSG, "Invalid name");
+                                    valid = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                    }while(!valid);
+
+                    //Deposit validation
+                    do{
+                        valid = true;
+                        System.out.print("\tInitial Deposit: ");
+                        initialDeposit = scanner.nextInt() ;
+                        scanner.nextLine();
+
+                        if (initialDeposit < 5000) {
+                            System.out.printf(ERROR_MSG, "Insufficient Amount");
+                            valid = false;
+                            continue;
+                        }
+
+
+                    }while(!valid);
+
+                    
+                    String[] newNames = new String[names.length + 1];
+                    int[] newDeposits = new int[deposits.length + 1];
+
+                    for (int i = 0; i < names.length; i++) {
+                        newNames[i] = names[i];
+                        newDeposits[i] = deposits[i];
+                    }
+                    newDeposits[newDeposits.length - 1] = initialDeposit;
+                    newNames[newNames.length - 1] = name;
+                    deposits = newDeposits;
+                    names = newNames;
+
+                    System.out.println();
+                    System.out.printf(SUCCESS_MSG, String.format("%s:%s has been saved successfully", initialDeposit, name));
+                    System.out.print("\tDo you want to continue adding? (Y/n)");
+                    if (scanner.nextLine().strip().toUpperCase().equals("Y")) continue;
+                    screen = DASHBOARD;
+                    break;
+                    
+
+            
+            }
+
+
+
+        }while(true);
+    }
+}
